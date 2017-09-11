62068d1bff8f96f53ca3687b32ad2c0b9672c04d
riceo <cube@sneakybox.com>
2017-09-11 19:22:16 +0100

    inital

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+
+   constructor(props, context) {
+    super();
+    this.state = {
+        time: []
+    };
+  }
+
+    componentWillMount() {
+    var self = this;
+    axios.get('http://localhost:8080/time')
+    .then(function (response) {
+      console.log(response)
+      self.setState({
+        time: response.data.time
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>The time is {this.state.time}</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
