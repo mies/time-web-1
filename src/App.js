39bdd7bb1c4297ce45cf706c19002b87218058a2
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-10-13 08:30:05 -0700

    add now

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <br />
           <img src={walter} className="walter-logo" alt="logo" />
-          <h2>The time is {this.state.time}</h2>
+          <h2>The time is now {this.state.time}</h2>
         </div>
       </div>
     );
